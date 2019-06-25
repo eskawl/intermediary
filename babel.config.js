4da85bd3d171a7d327aac85d2c01ce8d6d1c8bae
Shrutesh Pachineela <sruteshp@gmail.com>
2019-06-25 21:11:23 +0530

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "presets": [
+        ["@babel/preset-env", {
+            "targets": {
+                "browsers": "> 0.25%, not dead"
+            },
+            "exclude": ['@babel/plugin-transform-regenerator']
+        }]
+    ]
+}
