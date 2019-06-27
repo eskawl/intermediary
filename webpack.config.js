0760d21578ee164b3368df9caa53c70da1eb6ea3
Shrutesh Pachineela <sruteshp@gmail.com>
2019-06-27 20:31:40 +0530

    Add umd build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+const { resolve } = require("path");
+const babelConfig = require('./babel.config');
+
+module.exports = {
+  devtool: "none",
+  mode: "production",
+  entry: {
+    index: [resolve(__dirname, "src/intermediary.js")]
+  },
+  output: {
+    path: resolve(__dirname, "lib"),
+    library: "Intermediary",
+    libraryTarget: "umd",
+    filename: "intermediary.min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: babelConfig.presets,
+          }
+        }
+      }
+    ]
+  }
+};
