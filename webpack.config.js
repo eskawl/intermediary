4b32d8396a78bee133a797ae313e8535639e8bd8
eskawl <sruteshp@gmail.com>
2022-12-27 16:19:31 +0530

    Upgrade dependencies
    - Also move to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const { resolve } = require("path");
 const babelConfig = require('./babel.config');
 
 module.exports = {
-  devtool: "none",
   mode: "production",
   entry: {
     index: [resolve(__dirname, "src/intermediary.js")]
